7aba3ab2c1f1e0323ce40e40cb380dcd833c26db
Hiroaki Mikami <hiroaki8270@gmail.com>
2020-01-04 12:08:07 +0900

    Fix linting script

--- bin/lint.bash
+++ bin/lint.bash
@@ -2,4 +2,4 @@
 
 flake8 $(find $(dirname $0)/../nl2prog -name "*.py") \
        $(find $(dirname $0)/../test -name "*.py") \
-       $(find $(dirname $0)/../nl2code_examples -name "*.py")
+       $(find $(dirname $0)/../examples -name "*.py")
