1778ab3772909abfa750a160ff2ea4ba8a12551d
Hiroaki Mikami <hiroaki8270@gmail.com>
2020-02-01 15:04:20 +0900

    Fix setup.py

--- setup.py
+++ setup.py
@@ -13,4 +13,13 @@ setup(
     test_requires=extras["test"],
     colab_requires=extras["colab"],
     visualize_requires=extras["visualize"],
-    extras_require=extras)
+    extras_require=extras,
+    packages=["nl2prog", "nl2prog.dataset", "nl2prog.dataset.django",
+              "nl2prog.dataset.hearthstone", "nl2prog.dataset.nl2bash",
+              "nl2prog.encoders", "nl2prog.language", "nl2prog.language.bash",
+              "nl2prog.language.python", "nl2prog.metrics",
+              "nl2prog.metrics.python", "nl2prog.nn", "nl2prog.nn.functional",
+              "nl2prog.nn.nl2code", "nl2prog.nn.treegen", "nl2prog.nn.utils",
+              "nl2prog.utils", "nl2prog.utils.data",
+              "nl2prog.utils.data.nl2code",
+              "nl2prog.utils.nl2code", "nl2prog.utils.python"])
