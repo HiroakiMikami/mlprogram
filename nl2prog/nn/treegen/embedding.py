52e2770fb11b938c50d3a37b23b0ccb7b8254e3e
Hiroaki Mikami <hiroaki8270@gmail.com>
2020-03-15 15:37:02 +0900

    Refactor embedding classes in treegen

--- nl2prog/nn/treegen/embedding.py
+++ nl2prog/nn/treegen/embedding.py
@@ -1,54 +1,25 @@
 import torch
-from typing import Tuple
+import numpy as np
 import torch.nn as nn
 
 from nl2prog.nn import EmbeddingWithMask
 
 
-class QueryEmbedding(nn.Module):
-    def __init__(self, vocab_num: int, character_vocab_num: int,
-                 max_seq_num: int,
-                 embedding_dim: int, elem_embedding_dim: int,
-                 elem_feature_dim: int):
-        super(QueryEmbedding, self).__init__()
-        self.character_vocab_num = character_vocab_num
-        self.embed = nn.Embedding(vocab_num, embedding_dim)
-        self.elem_embed = EmbeddingWithMask(character_vocab_num,
-                                            elem_embedding_dim,
-                                            character_vocab_num)
-        self.elem_to_seq = nn.Conv1d(elem_embedding_dim, elem_feature_dim,
-                                     max_seq_num, bias=False)
+class ElementEmbedding(nn.Module):
+    def __init__(self, embed: nn.Module, max_elem_num: int,
+                 elemwise_embedding_dim: int,
+                 embedding_dim: int):
+        super(ElementEmbedding, self).__init__()
+        self.embed = embed
+        self.elem_to_seq = nn.Conv1d(elemwise_embedding_dim, embedding_dim,
+                                     max_elem_num, bias=False)
 
-    def forward(self, sequence: torch.Tensor, char_seq: torch.Tensor) \
-            -> Tuple[torch.Tensor, torch.Tensor]:
-        """
-        Parameters
-        ----------
-        sequence: torch.Tensor
-            The shape is (L, N). where L is the sequence length and
-            N is the batch size.
-        char_seq: torch.Tensor
-            The shape is (L, N, max_seq_len). where L is the sequence length
-            and N is the batch size. The padding value should be -1.
-
-        Returns
-        -------
-        seq_embed: torch.Tensor
-            The shape is (L, N, embedding_dim). where L is the sequence length
-            and N is the batch size.
-        char_seq_embed: torch.Tensor
-            The shape is (L, N, elem_feature_dim). where L is the sequence
-            length and N is the batch size.
-        """
-        L, N = sequence.shape[:2]
-        n = char_seq.shape[2]
-        seq_embed = self.embed(sequence)
-        char_seq = char_seq + (char_seq == -1) * (self.character_vocab_num + 1)
-        char_seq_embed = self.elem_embed(char_seq)
-        char_seq_embed = self.elem_to_seq(
-            char_seq_embed.reshape(L * N, n, -1).permute(0, 2, 1)
-        ).reshape(L, N, -1)
-        return seq_embed, char_seq_embed
+    def forward(self, input):
+        embed = self.embed(input)
+        dims = embed.shape
+        embed = embed.reshape(np.prod(dims[:-2]), dims[-2], dims[-1]) \
+            .permute(0, 2, 1)
+        return self.elem_to_seq(embed).reshape(*dims[:-2], -1)
 
 
 class ActionEmbedding(nn.Module):
@@ -90,69 +61,45 @@ class ActionEmbedding(nn.Module):
         return self.rule_embed(rule_seq) + self.token_embed(token_seq)
 
 
-class RuleEmbedding(nn.Module):
-    def __init__(self, rule_num: int, token_num: int, node_type_num: int,
-                 max_arity: int,
-                 embedding_dim: int, elem_embedding_dim: int,
-                 elem_feature_dim: int):
-        super(RuleEmbedding, self).__init__()
-        self.rule_num = rule_num
+class ActionSignatureEmbedding(nn.Module):
+    def __init__(self, token_num: int, node_type_num: int,
+                 embedding_dim: int):
+        super(ActionSignatureEmbedding, self).__init__()
         self.token_num = token_num
         self.node_type_num = node_type_num
+        self.node_type_embed = EmbeddingWithMask(
+            node_type_num, embedding_dim, node_type_num)
+        self.token_embed = EmbeddingWithMask(
+            token_num + 1, embedding_dim, token_num + 1)
 
-        self.action_embed = ActionEmbedding(rule_num, token_num, embedding_dim)
-        self.elem_node_type_embed = \
-            EmbeddingWithMask(node_type_num, elem_embedding_dim, node_type_num)
-        self.elem_token_embed = \
-            EmbeddingWithMask(token_num + 1, elem_embedding_dim, token_num + 1)
-        self.elem_to_seq = nn.Conv1d(elem_embedding_dim, elem_feature_dim,
-                                     max_arity + 1, bias=False)
-
-    def forward(self, sequence: torch.Tensor, elem_seq: torch.Tensor) \
-            -> Tuple[torch.Tensor, torch.Tensor]:
+    def forward(self, signature: torch.Tensor) -> torch.Tensor:
         """
         Parameters
         ----------
-        sequence: torch.Tensor
-            The shape is (L, N, 3). where L is the sequence length and
-            N is the batch size. The padding value should be -1.
-            [:, :, 0] represent the rule IDs, [:, :, 1] represent the token
-            IDs, [:, :, 2] represent the indexes of the queries.
-        elem_seq: torch.Tensor
-            The shape is (L, N, max_arity + 1, 3). where L is the
-            sequence length and N is the batch size.
+        signature: torch.Tensor
+            The shape is (-1, 3)
             The padding value should be -1.
-            [:, :, :, 0] represent the IDs of the node types, [:, :, :, 1]
-            represent the token IDs, [:, :, :, 2] represent the indexes of the
+            [:, 0] represent the IDs of the node types, [:, 1]
+            represent the token IDs, [:, 2] represent the indexes of the
             queries.
 
         Returns
         -------
-        seq_embed: torch.Tensor
-            The shape is (L, N, embedding_dim). where L is the sequence length
-            and N is the batch size.
-        elem_seq_embed: torch.Tensor
-            The shape is (L, N, elem_feature_dim). where L is the sequence
-            length and N is the batch size.
+        embed: torch.Tensor
+            The shape is (-1, feature_dim).
         """
-        L, N = sequence.shape[:2]
-        n = elem_seq.shape[2]
-        seq_embed = self.action_embed(sequence)
+        dims = signature.shape
+        signature = signature.reshape(-1, dims[-1])
 
-        elem_node_type_seq = elem_seq[:, :, :, 0]
-        elem_node_type_seq = \
-            elem_node_type_seq + \
-            (elem_node_type_seq == -1) * (self.node_type_num + 1)
-        elem_token_seq = elem_seq[:, :, :, 1]
-        elem_copy_seq = (elem_token_seq == -1) * (elem_seq[:, :, :, 2] != -1)
-        elem_token_seq = elem_token_seq + elem_copy_seq * (self.token_num + 2)
-        elem_token_seq = \
-            elem_token_seq + (elem_token_seq == -1) * (self.token_num + 2)
+        node_type_seq = signature[:, 0]
+        node_type_seq = node_type_seq + \
+            (node_type_seq == -1) * (self.node_type_num + 1)
+        token_seq = signature[:, 1]
+        copy_seq = (token_seq == -1) * (signature[:, 2] != -1)
+        token_seq = token_seq + copy_seq * (self.token_num + 2)
+        token_seq = token_seq + \
+            (token_seq == -1) * (self.token_num + 2)
 
-        elem_seq_emebed = \
-            self.elem_node_type_embed(elem_node_type_seq) + \
-            self.elem_token_embed(elem_token_seq)
-        elem_seq_embed = self.elem_to_seq(
-            elem_seq_emebed.reshape(L * N, n, -1).permute(0, 2, 1)
-        ).reshape(L, N, -1)
-        return seq_embed, elem_seq_embed
+        embed = self.node_type_embed(node_type_seq) + \
+            self.token_embed(token_seq)
+        return embed.reshape(*dims[:-1], -1)
