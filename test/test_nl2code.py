59bbd591d1ee0447265529d972d4f4fa5ce86ada
Hiroaki Mikami <hiroaki8270@gmail.com>
2020-04-29 14:49:03 +0900

    Use functions for gin configs to test NL2Code

--- test/test_nl2code.py
+++ test/test_nl2code.py
@@ -1,32 +1,30 @@
 import unittest
-from dummy_dataset import is_subtype, train_dataset, test_dataset
+from dummy_dataset import is_subtype, prepare_dataset
 from typing import List
-from tqdm import trange
+import logging
+import sys
+import tempfile
+import os
 
 import torch
 import torch.optim as optim
-from torch.utils.data import DataLoader
-
-from torchnlp.encoders import LabelEncoder
-
-from nl2prog.encoders import ActionSequenceEncoder
-from nl2prog.utils \
-    import Query, synthesize as _synthesize, evaluate, \
-    CommonBeamSearchSynthesizer
+from nl2prog.gin import nl2prog, nl2code, workspace, optimizer
+from nl2prog.utils import Query, CommonBeamSearchSynthesizer
 from nl2prog.language.action \
     import ast_to_action_sequence as to_seq, ActionOptions
 from nl2prog.utils.data \
-    import get_samples, to_eval_dataset, get_words, \
-    Collate, CollateGroundTruth, collate_none, CollateNlFeature
+    import Collate, CollateGroundTruth, collate_none, CollateNlFeature
 from nl2prog.utils.data.nl2code \
     import CollateInput, CollateActionSequence, \
     CollateState, split_states
 from nl2prog.utils.transform \
     import TransformDataset, TransformCode, TransformGroundTruth
 from nl2prog.utils.transform.nl2code import TransformQuery, TransformEvaluator
-from nl2prog.nn import Loss, Accuracy as Acc
+from nl2prog.nn import Loss
 from nl2prog.nn.nl2code import TrainModel
 from nl2prog.metrics import Accuracy
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, force=True)
 
 
 def tokenize_query(str: str) -> Query:
@@ -44,11 +42,29 @@ def tokenize_token_2(token: str) -> List[str]:
 
 
 class TestNL2Code(unittest.TestCase):
-    def evaluate(self, model, options, dataset):
-        test_dataset = to_eval_dataset(dataset)
-        encoder, model = model
-        transform_input = TransformQuery(tokenize_query, encoder[0])
-        transform_evaluator = TransformEvaluator(encoder[1], train=False)
+    def prepare_encoder(self, to_action_sequence):
+        dataset = workspace.get("dataset")["train"]
+        nl2code.prepare_encoder(dataset, 20, 20,
+                                lambda x: x, to_action_sequence,
+                                tokenize_query, tokenize_token)
+
+    def prepare_model(self, model_key):
+        qencoder = workspace.get("query_encoder")
+        aencoder = workspace.get("action_sequence_encoder")
+        model = TrainModel(qencoder, aencoder, 256, 64, 256, 64, 0.0)
+        workspace.put(model_key, model)
+
+    def prepare_optimizer(self, optimizer_key):
+        opt = optimizer.create_optimizer(optim.Adam, workspace.get("model"))
+        workspace.put(optimizer_key, opt)
+
+    def prepare_synthesizer(self, synthesizer_key, options):
+        qencoder = workspace.get("query_encoder")
+        aencoder = workspace.get("action_sequence_encoder")
+        self.prepare_model("model")
+        model = workspace.get("model")
+        transform_input = TransformQuery(tokenize_query, qencoder)
+        transform_evaluator = TransformEvaluator(aencoder, train=False)
         synthesizer = CommonBeamSearchSynthesizer(
             5, transform_input, transform_evaluator,
             CollateInput(torch.device("cpu")),
@@ -57,90 +73,88 @@ class TestNL2Code(unittest.TestCase):
             CollateNlFeature(torch.device("cpu")),
             collate_none, split_states,
             model.input_reader, model.action_sequence_reader, model.decoder,
-            model.predictor, encoder[1], is_subtype,
+            model.predictor, aencoder, is_subtype,
             options=options, max_steps=20)
+        workspace.put(synthesizer_key, synthesizer)
 
-        def synthesize(query: str):
-            return _synthesize(query, synthesizer)
-
-        accuracy = Accuracy(lambda x: x, lambda x: x)
-        metrics = {"accuracy": accuracy}
-        result = evaluate(test_dataset, synthesize,
-                          top_n=[1], metrics=metrics)
-        return result
-
-    def train(self, options, dataset, tokenize_token):
-        def to_action_sequence(ast):
-            return to_seq(ast, options=options, tokenizer=tokenize_token)
-
-        words = get_words(dataset, tokenize_query)
-        samples = get_samples(dataset, tokenize_token, to_action_sequence)
-        qencoder = LabelEncoder(words, 2)
-        aencoder = ActionSequenceEncoder(samples, 2)
-
+    def transform_cls(self, to_action_sequence):
+        qencoder = workspace.get("query_encoder")
+        aencoder = workspace.get("action_sequence_encoder")
         tquery = TransformQuery(tokenize_query, qencoder)
         tcode = TransformCode(to_action_sequence)
         teval = TransformEvaluator(aencoder)
         tgt = TransformGroundTruth(aencoder)
-        transform = TransformDataset(tquery, tcode, teval, tgt)
+        return TransformDataset(tquery, tcode, teval, tgt)
 
-        train_dataset = transform(dataset)
-        model = TrainModel(qencoder, aencoder, 256, 64, 256, 64, 0.0)
-        optimizer = optim.Adam(model.parameters())
-        loss_function = Loss()
-        acc_function = Acc()
+    def evaluate(self, options, dir):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            nl2prog.evaluate(
+                "dataset", "synthesizer",
+                set(["query_encoder", "action_sequence_encoder"]),
+                dir, tmpdir, dir,
+                lambda x: prepare_dataset(x, 1),
+                lambda key: self.prepare_synthesizer(key, options),
+                {"accuracy": Accuracy(lambda x: x, lambda x: x)},
+                (5, "accuracy"), top_n=[5]
+            )
+        results = torch.load(os.path.join(dir, "results.pt"))
+        return results["valid"]
 
-        for _ in trange(100):
-            loader = DataLoader(train_dataset, 1, shuffle=True,
-                                collate_fn=Collate(
-                                    CollateInput(torch.device("cpu")),
-                                    CollateActionSequence(torch.device("cpu")),
-                                    collate_none,
-                                    CollateGroundTruth(torch.device("cpu"))))
-            avg_acc = 0
-            for input, action_sequence, query, ground_truth in loader:
-                rule_prob, token_prob, copy_prob = model(
-                    input, action_sequence, query)
-                loss = loss_function(rule_prob, token_prob, copy_prob,
-                                     ground_truth)
-                acc = acc_function(rule_prob, token_prob, copy_prob,
-                                   ground_truth)
-                model.zero_grad()
-                loss.backward()
-                optimizer.step()
-                avg_acc += acc.item() / len(loader)
-            if avg_acc == 1.0:
-                break
-        return (qencoder, aencoder), model
+    def train(self, options, tokenize_token, output_dir):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            def to_action_sequence(ast):
+                return to_seq(ast, options=options, tokenizer=tokenize_token)
+            collate_fn = Collate(
+                CollateInput(torch.device("cpu")),
+                CollateActionSequence(torch.device("cpu")),
+                collate_none,
+                CollateGroundTruth(torch.device("cpu")))
+
+            nl2prog.train(
+                "dataset", "model", "optimizer",
+                set(["query_encoder", "action_sequence_encoder"]),
+                tmpdir, output_dir,
+                lambda x: prepare_dataset(x, 10),
+                lambda: self.prepare_encoder(to_action_sequence),
+                self.prepare_model, self.prepare_optimizer,
+                lambda: self.transform_cls(to_action_sequence),
+                Loss(), lambda *args: -Loss()(*args), collate_fn, 1, 10,
+                num_models=1
+            )
 
     def test_default_settings(self):
         torch.manual_seed(0)
         options = ActionOptions(True, True)
-        model = self.train(options, train_dataset, tokenize_token)
-        results = self.evaluate(model, options, test_dataset)
-        self.assertAlmostEqual(1.0, results.metrics[1]["accuracy"])
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self.train(options, tokenize_token, tmpdir)
+            results = self.evaluate(options, tmpdir)
+        self.assertAlmostEqual(1.0, results.metrics[5]["accuracy"])
 
-        model = self.train(options, train_dataset, tokenize_token_2)
-        results = self.evaluate(model, options, test_dataset)
-        self.assertAlmostEqual(1.0, results.metrics[1]["accuracy"])
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self.train(options, tokenize_token_2, tmpdir)
+            results = self.evaluate(options, tmpdir)
+        self.assertAlmostEqual(1.0, results.metrics[5]["accuracy"])
 
     def test_not_split_nonterminals(self):
         torch.manual_seed(0)
         options = ActionOptions(True, False)
-        model = self.train(options, train_dataset, tokenize_token)
-        results = self.evaluate(model, options, test_dataset)
-        self.assertAlmostEqual(1.0, results.metrics[1]["accuracy"])
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self.train(options, tokenize_token, tmpdir)
+            results = self.evaluate(options, tmpdir)
+        self.assertAlmostEqual(1.0, results.metrics[5]["accuracy"])
 
     def test_not_retain_variadic_args(self):
         torch.manual_seed(0)
         options = ActionOptions(False, True)
-        model = self.train(options, train_dataset, tokenize_token)
-        results = self.evaluate(model, options, test_dataset)
-        self.assertAlmostEqual(1.0, results.metrics[1]["accuracy"])
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self.train(options, tokenize_token, tmpdir)
+            results = self.evaluate(options, tmpdir)
+        self.assertAlmostEqual(1.0, results.metrics[5]["accuracy"])
 
-        model = self.train(options, train_dataset, tokenize_token_2)
-        results = self.evaluate(model, options, test_dataset)
-        self.assertAlmostEqual(1.0, results.metrics[1]["accuracy"])
+        with tempfile.TemporaryDirectory() as tmpdir:
+            self.train(options, tokenize_token_2, tmpdir)
+            results = self.evaluate(options, tmpdir)
+        self.assertAlmostEqual(1.0, results.metrics[5]["accuracy"])
 
 
 if __name__ == "__main__":
