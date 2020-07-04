b5b2caf6593ce2a739cc83c91ea53ccc5a21edd5
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-07-04 14:41:07 +0900

    Optimize ActionSequenceSampler performance

--- mlprogram/samplers/action_sequence_sampler.py
+++ mlprogram/samplers/action_sequence_sampler.py
@@ -13,7 +13,7 @@ from mlprogram.actions import ActionSequence
 from mlprogram.asts import AST, Node
 from mlprogram.samplers import SamplerState, Sampler
 from mlprogram.nn.utils.rnn import PaddedSequenceWithMask
-from mlprogram.utils import TopKElement
+from mlprogram.utils import TopKElement, Token
 from mlprogram.utils.data import Collate
 
 logger = logging.getLogger(__name__)
@@ -102,98 +102,123 @@ class ActionSequenceSampler(Sampler[Dict[str, Any], AST, Dict[str, Any]],
         next_state_list = self.collate.split(next_states)
         return rule_pred, token_pred, copy_pred, next_state_list
 
-    def all_actions(
-        self, states: List[SamplerState[Dict[str, Any]]]) \
+    def enumerate_samples_per_state(self,
+                                    rule_pred: torch.Tensor,
+                                    token_pred: torch.Tensor,
+                                    copy_pred: torch.Tensor,
+                                    next_state: Dict[str, Any],
+                                    state: SamplerState[Dict[str, Any]],
+                                    k: Optional[int] = None) \
             -> Generator[Tuple[float,
                                SamplerState[Dict[str, Any]],
                                Dict[str, Any], Action],
                          None, None]:
-        rule_pred, token_pred, copy_pred, next_states = \
-            self.batch_infer(states)
-        for i, state in enumerate(states):
-            state = states[i]
-            head = state.state["action_sequence"].head
-            is_token = False
-            if head is None:
-                continue
-            head_field = \
-                cast(ExpandTreeRule, cast(
-                    ApplyRule,
-                    state.state["action_sequence"].action_sequence[head.action]
-                ).rule).children[head.field][1]
-            if head_field.constraint == NodeConstraint.Token:
-                is_token = True
-            if is_token:
-                # Generate token
-                probs: Dict[V, float] = {}
-                # predefined token
-                for j in range(1, min(self.encoder._token_encoder.vocab_size,
-                                      token_pred.shape[1])):
-                    # 0 is unknown token
-                    p = token_pred[i, j].item()
-                    token = self.encoder._token_encoder.vocab[j]
-                    t = self.get_token_type(token)
-                    if t is not None and \
-                            not self.is_subtype(t, head_field.type_name):
-                        continue
-                    if token not in probs:
-                        probs[token] = 0.0
-                    probs[token] += p
-                # reference
-                for j in range(min(copy_pred.shape[1],
-                                   len(state.state["reference"]))):
-                    p = copy_pred[i, j].item()
-                    token = state.state["reference"][j].value
-                    t = state.state["reference"][j].type_name
-                    if t is not None and \
-                            not self.is_subtype(t, head_field.type_name):
-                        continue
-                    if token not in probs:
-                        probs[token] = 0.0
-                    probs[token] += p
-
-                # Convert to log probs
-                for token, p in probs.items():
-                    if p < self.eps:
-                        lp = np.log(self.eps)
-                    else:
-                        lp = np.log(p)
-                    yield (state.score + lp, state, next_states[i],
-                           GenerateToken(token))
+        head = state.state["action_sequence"].head
+        is_token = False
+        if head is None:
+            return
+        head_field = \
+            cast(ExpandTreeRule, cast(
+                ApplyRule,
+                state.state["action_sequence"].action_sequence[head.action]
+            ).rule).children[head.field][1]
+        if head_field.constraint == NodeConstraint.Token:
+            is_token = True
+        if is_token:
+            # Generate token
+            if len(state.state["reference"]) == 0:
+                ref_ids = torch.tensor([]).long()
             else:
-                # Apply rule
-                for j in range(1, min(rule_pred.shape[1],
-                                      self.encoder._rule_encoder.vocab_size)):
-                    # 0 is unknown rule
-                    p = rule_pred[i, j].item()
-                    if p < self.eps:
-                        lp = np.log(self.eps)
-                    else:
-                        lp = np.log(p)
-                    rule = self.encoder._rule_encoder.vocab[j]
-                    if isinstance(rule, ExpandTreeRule):
-                        if rule.parent.type_name is not None and \
-                            self.is_subtype(
-                                rule.parent.type_name,
-                                head_field.type_name):
-                            yield (state.score + lp, states[i],
-                                   next_states[i], ApplyRule(rule))
-                    else:
-                        # CloseVariadicFieldRule
-                        if self.options.retain_variadic_fields and \
-                            head_field is not None and \
-                            head_field.constraint == \
-                                NodeConstraint.Variadic:
-                            yield (state.score + lp, states[i], next_states[i],
-                                   ApplyRule(rule))
+                ref_ids = self.encoder._token_encoder.batch_encode(
+                    [token.value for token in state.state["reference"]]
+                )
+            tokens = list(self.encoder._token_encoder.vocab) + \
+                state.state["reference"]
+            # the score will be merged into predefined token
+            token_pred[ref_ids] += copy_pred  # merge token and copy pred
+            copy_pred[ref_ids != 0] = 0.0
+            pred = torch.cat([token_pred, copy_pred], dim=0)
+
+            n_token = 0
+            # 0 is unknown token
+            _, indices = torch.sort(pred[1:], descending=True)
+            for j in range(len(indices)):
+                # Finish enumeration
+                if n_token == k:
+                    return
+
+                x = indices[j] + 1
+                p = pred[x].item()
+                token = tokens[x]
+
+                if isinstance(token, Token):
+                    t = token.type_name
+                    token = token.value
+                else:
+                    t = self.get_token_type(token)
+                if t is not None and \
+                        not self.is_subtype(t, head_field.type_name):
+                    continue
+
+                if p < self.eps:
+                    lp = np.log(self.eps)
+                else:
+                    lp = np.log(p)
+                n_token += 1
+                yield (state.score + lp, state, next_state,
+                       GenerateToken(token))
+
+        else:
+            # Apply rule
+            # 0 is unknown rule
+            _, indices = torch.sort(rule_pred[1:], descending=True)
+            n_rule = 0
+            for j in range(min(rule_pred.shape[0],
+                               self.encoder._rule_encoder.vocab_size) - 1):
+                # Finish enumeration
+                if n_rule == k:
+                    return
+
+                x = indices[j] + 1
+                p = rule_pred[x].item()
+                if p < self.eps:
+                    lp = np.log(self.eps)
+                else:
+                    lp = np.log(p)
+                rule = self.encoder._rule_encoder.vocab[x]
+                if isinstance(rule, ExpandTreeRule):
+                    if rule.parent.type_name is not None and \
+                        self.is_subtype(
+                            rule.parent.type_name,
+                            head_field.type_name):
+                        n_rule += 1
+                        yield (state.score + lp, state,
+                               next_state, ApplyRule(rule))
+                else:
+                    # CloseVariadicFieldRule
+                    if self.options.retain_variadic_fields and \
+                        head_field is not None and \
+                        head_field.constraint == \
+                            NodeConstraint.Variadic:
+                        n_rule += 1
+                        yield (state.score + lp, state, next_state,
+                               ApplyRule(rule))
 
     def top_k_samples(
         self, states: List[SamplerState[Dict[str, Any]]], k: int) \
             -> Generator[SamplerState[Dict[str, Any]], None, None]:
         self.module.eval()
+        rule_pred, token_pred, copy_pred, next_states = \
+            self.batch_infer(states)
         topk = TopKElement(k)
-        for score, state, new_state, action in self.all_actions(states):
-            topk.add(score, (state, new_state, action))
+        for i, state in enumerate(states):
+            for elem in self.enumerate_samples_per_state(rule_pred[i],
+                                                         token_pred[i],
+                                                         copy_pred[i],
+                                                         next_states[i],
+                                                         state,
+                                                         k=k):
+                topk.add(elem[0], tuple(elem[1:]))
 
         # Instantiate top-k hypothesis
         for score, (state, new_state, action) in topk.elements:
@@ -208,7 +233,14 @@ class ActionSequenceSampler(Sampler[Dict[str, Any], AST, Dict[str, Any]],
             -> Generator[SamplerState[Dict[str, Any]],
                          None, None]:
         self.module.eval()
-        actions = list(self.all_actions([state]))
+
+        rule_pred, token_pred, copy_pred, next_states = \
+            self.batch_infer([state])
+        actions = list(self.enumerate_samples_per_state(rule_pred[0],
+                                                        token_pred[0],
+                                                        copy_pred[0],
+                                                        next_states[0],
+                                                        state))
         # log_prob -> prob
         probs = [np.exp(score) for score, _, _, _ in actions]
         # normalize
