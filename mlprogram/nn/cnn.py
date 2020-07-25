33813f273bfac20fdcda25e0dbc69e8686d27dcc
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-07-25 18:37:43 +0900

    Fix CNN2d to handle empty tensor

--- mlprogram/nn/cnn.py
+++ mlprogram/nn/cnn.py
@@ -1,17 +1,17 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 from collections import OrderedDict
+from math import prod
 
 
-def block_2d(in_channel: int, out_channel: int, n_conv: int, pool: int):
+def block_2d(in_channel: int, out_channel: int, n_conv: int):
     modules = []
     dim = in_channel
     for i in range(n_conv):
         modules.append((f"conv{i}", nn.Conv2d(dim, out_channel, 3, padding=1)))
         dim = out_channel
         modules.append((f"relu{i}", nn.ReLU()))
-    if pool != 1:
-        modules.append(("pool", nn.MaxPool2d(2)))
     return nn.Sequential(OrderedDict(modules))
 
 
@@ -24,20 +24,31 @@ class CNN2d(nn.Module):
         modules = []
         dim = in_channel
         modules.append(("block0", block_2d(
-            dim, hidden_channel, n_conv_per_block, pool)))
+            dim, hidden_channel, n_conv_per_block)))
         dim = hidden_channel
         for i in range(n_block - 2):
             modules.append((f"block{i + 1}", block_2d(
-                dim, hidden_channel, n_conv_per_block, pool)))
+                dim, hidden_channel, n_conv_per_block)))
             dim = hidden_channel
         modules.append((f"block{n_block - 1}", block_2d(
-            dim, out_channel, n_conv_per_block, pool)))
-        self.module = nn.Sequential(OrderedDict(modules))
+            dim, out_channel, n_conv_per_block)))
+        for name, module in modules:
+            self.add_module(name, module)
+        self.keys = list([key for key, _ in modules])
+        self.pool = pool
         self.flatten = flatten
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        out = self.module(x)
+        out = x
+        modules = dict(self.named_modules())
+        for key in self.keys:
+            out = modules[key](out)
+            if self.pool != 1 and len(out) != 0:
+                out = F.max_pool2d(out, self.pool)
+            elif len(out) == 0:
+                N, C, H, W = out.shape
+                out = out.reshape(N, C, H // self.pool, W // self.pool)
         if self.flatten:
-            return out.reshape(out.shape[0], -1)
+            return out.reshape(out.shape[0], prod(out.shape[1:]))
         else:
             return out
--- test/nn/test_cnn.py
+++ test/nn/test_cnn.py
@@ -10,11 +10,11 @@ class TestCNN2d(unittest.TestCase):
         params = dict(cnn.named_parameters())
         self.assertEqual(4, len(params))
         self.assertEqual((3, 1, 3, 3),
-                         params["module.block0.conv0.weight"].shape)
-        self.assertEqual((3,), params["module.block0.conv0.bias"].shape)
+                         params["block0.conv0.weight"].shape)
+        self.assertEqual((3,), params["block0.conv0.bias"].shape)
         self.assertEqual((2, 3, 3, 3),
-                         params["module.block1.conv0.weight"].shape)
-        self.assertEqual((2,), params["module.block1.conv0.bias"].shape)
+                         params["block1.conv0.weight"].shape)
+        self.assertEqual((2,), params["block1.conv0.bias"].shape)
 
     def test_shape(self):
         cnn = CNN2d(1, 2, 3, 1, 2, 2)
@@ -25,6 +25,11 @@ class TestCNN2d(unittest.TestCase):
         cnn = CNN2d(1, 2, 3, 1, 2, 2, flatten=False)
         out = cnn(torch.rand(1, 1, 8, 8))
         self.assertEqual((1, 2, 2, 2), out.shape)
+
+    def test_shape_empty(self):
+        cnn = CNN2d(1, 2, 3, 1, 2, 2)
+        out = cnn(torch.rand(0, 1, 8, 8))
+        self.assertEqual((0, 8), out.shape)
 
 
 if __name__ == "__main__":
