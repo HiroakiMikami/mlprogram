a38f1c6c2d9804b0f9f494dfa59af65c1a2e46e8
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-07-25 17:01:06 +0900

    Fix IoU

--- mlprogram/metrics/iou.py
+++ mlprogram/metrics/iou.py
@@ -1,15 +1,29 @@
 import numpy as np
-from typing import Dict, Any
+from typing import Dict, Any, Generic, TypeVar, cast, List, Tuple
 from mlprogram.metrics import Metric
+from mlprogram.interpreters import Interpreter
+from mlprogram.utils import Reference
+
+Code = TypeVar("Code")
 
 
-class Iou(Metric[np.array]):
+class Iou(Metric[Code], Generic[Code]):
+    def __init__(self, interpreter: Interpreter[Code, np.array],
+                 reference: bool = False):
+        self.interpreter = interpreter
+        self.reference = reference
+
     def __call__(self, input: Dict[str, Any],
-                 actual: np.array) -> float:
-        ground_truth = input["ground_truth"]
+                 actual: Code) -> float:
+        test_case = input["test_case"]
+        if self.reference:
+            actual_ref = cast(List[Tuple[Reference, Any]], actual)
+            output = self.interpreter.eval_references(actual_ref)[
+                actual_ref[-1][0]]
+        else:
+            output = self.interpreter.eval(actual)
         iou = 0.0
-        for gt in ground_truth:
-            intersection = (gt & actual).astype(np.float).sum()
-            union = (gt | actual).astype(np.float).sum()
-            iou = max(iou, (intersection / union).item())
+        intersection = (test_case & output).astype(np.float).sum()
+        union = (test_case | output).astype(np.float).sum()
+        iou = max(iou, (intersection / union).item())
         return iou
