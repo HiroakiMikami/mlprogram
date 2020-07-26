4a1c4ce85d9222fb44ca470eb42a4532cb94e893
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-07-26 11:22:26 +0900

    Fix load configs

--- mlprogram/entrypoint/parse.py
+++ mlprogram/entrypoint/parse.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 from typing import Dict, Any
 from pytorch_pfn_extras.config import Config
@@ -8,7 +9,27 @@ def parse_config(configs: Dict[str, Any]) -> Config:
     return Config(configs, types)
 
 
-def parse_config_file(file: str) -> Config:
+def load_config(file: str) -> Dict[str, Any]:
     with open(file) as f:
-        configs = yaml.load(f)
-    return parse_config(configs)
+        orig = yaml.load(f)
+    dir = os.path.dirname(file)
+    if "imports" in orig:
+        imports = orig.pop("imports")
+    else:
+        imports = []
+    configs = {}
+    for subfile in imports:
+        subconfig = load_config(os.path.join(dir, subfile))
+        for key, value in subconfig.items():
+            configs[key] = value
+    with open(file) as f:
+        main = yaml.load(f)
+    for key, value in main.items():
+        if key == "imports":
+            continue
+        configs[key] = value
+    return configs
+
+
+def parse_config_file(file: str) -> Config:
+    return parse_config(load_config(file))
