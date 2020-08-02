5c47a529c85a2a27961c54913356a355f0b5f4b1
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-08-02 14:19:30 +0900

    Fix encoder for PbE w/ REPL

--- mlprogram/nn/pbe_with_repl/encoder.py
+++ mlprogram/nn/pbe_with_repl/encoder.py
@@ -5,14 +5,29 @@ from mlprogram.nn.utils.rnn import PaddedSequenceWithMask
 
 
 class Encoder(nn.Module):
-    def __init__(self, reduction: str = "sum"):
+    def __init__(self, module: nn.Module, reduction: str = "sum"):
         assert reduction == "sum" or reduction == "mean"
         super().__init__()
+        self.module = module
         self.reduction = reduction
 
     def forward(self, entry: Dict[str, Any]) -> Dict[str, Any]:
+        processed_input = cast(torch.Tensor, entry["processed_input"])
+        variables = cast(PaddedSequenceWithMask, entry["variables"])
+        if len(variables.data) != 0:
+            processed_input = \
+                processed_input.unsqueeze(0).expand(variables.data.shape)
+        else:
+            processed_input = torch.zeros_like(variables.data)
+        f = torch.cat([processed_input, variables.data], dim=2)
+        L, B = f.shape[:2]
+        vfeatures = self.module(f.reshape(L * B, *f.shape[2:]))
+        vfeatures = vfeatures.reshape(L, B, *vfeatures.shape[1:])
+
         in_feature = cast(torch.Tensor, entry["input_feature"])
-        features = cast(PaddedSequenceWithMask, entry["reference_features"])
+        features = PaddedSequenceWithMask(
+            vfeatures * variables.mask.unsqueeze(2),
+            variables.mask)
         B = in_feature.shape[0]
         C = in_feature.shape[1:]
         if features.data.numel() == 0:
@@ -20,12 +35,12 @@ class Encoder(nn.Module):
                                         dtype=in_feature.dtype)
         entry["reference_features"] = features
 
-        reduced_feature = \
-            (features.data * features.mask.unsqueeze(2)).sum(dim=0)
+        reduced_feature = features.data.sum(dim=0)
         if self.reduction == "mean":
             in_feature = in_feature.float()
+            n_samples = features.mask.sum(dim=0).float().reshape(B, 1)
             reduced_feature = \
-                reduced_feature / features.mask.sum(dim=0).float()
+                reduced_feature / n_samples
         entry["variable_feature"] = reduced_feature
         entry["input_feature"] = torch.cat([in_feature, reduced_feature],
                                            dim=1)
--- test/nn/pbe_with_repl/test_encoder.py
+++ test/nn/pbe_with_repl/test_encoder.py
@@ -7,16 +7,17 @@ from mlprogram.nn.pbe_with_repl import Encoder
 
 class TestEncoder(unittest.TestCase):
     def test_parameters(self):
-        encoder = Encoder()
+        encoder = Encoder(torch.nn.ReLU())
         self.assertEqual(0, len(list(encoder.parameters())))
 
     def test_shape(self):
-        encoder = Encoder()
+        encoder = Encoder(torch.nn.Linear(2, 1))
         output = encoder({
+            "processed_input": torch.rand(1, 1),
             "input_feature": torch.arange(2).reshape(2, 1),
-            "reference_features": pad_sequence([torch.arange(3).reshape(3, 1),
-                                                torch.arange(1).reshape(1, 1)
-                                                ])
+            "variables": pad_sequence([torch.rand(3, 1),
+                                       torch.rand(1, 1),
+                                       ])
         })
         self.assertEqual((2, 2), output["input_feature"].shape)
         self.assertEqual((3, 2, 1), output["reference_features"].data.shape)
@@ -26,42 +27,48 @@ class TestEncoder(unittest.TestCase):
         ))
 
     def test_empty_sequence(self):
-        encoder = Encoder()
+        encoder = Encoder(torch.nn.Linear(2, 1))
         output = encoder({
+            "processed_input": torch.rand(1, 1),
             "input_feature": torch.arange(1).reshape(1, 1),
-            "reference_features": pad_sequence([torch.arange(0).reshape(0)])
+            "variables": pad_sequence([torch.rand(0, 1)])
         })
         self.assertEqual((1, 2), output["input_feature"].shape)
         self.assertEqual((1, 1), output["variable_feature"].shape)
         self.assertEqual((0, 1, 1), output["reference_features"].data.shape)
 
     def test_mean(self):
-        encoder = Encoder()
-        mencoder = Encoder("mean")
+        module = torch.nn.Linear(2, 1)
+        encoder = Encoder(module)
+        mencoder = Encoder(module, "mean")
+        input = torch.rand(2, 1)
+        variables = pad_sequence([torch.rand(3, 1),
+                                  torch.rand(1, 1)
+                                  ])
         output = encoder({
+            "processed_input": input,
             "input_feature": torch.arange(2).reshape(2, 1),
-            "reference_features": pad_sequence([torch.arange(3).reshape(3, 1),
-                                                torch.arange(1).reshape(1, 1)
-                                                ])
+            "variables": variables
         })
         mean = mencoder({
+            "processed_input": input,
             "input_feature": torch.arange(2).reshape(2, 1),
-            "reference_features": pad_sequence([torch.arange(3).reshape(3, 1),
-                                                torch.arange(1).reshape(1, 1)
-                                                ])
+            "variables": variables
         })
-        self.assertTrue(np.array_equal(
-            output["input_feature"][:, 1] / torch.tensor([3.0, 1.0]),
-            mean["input_feature"][:, 1],
+        self.assertTrue(np.allclose(
+            (output["input_feature"][:, 1] /
+             torch.tensor([3.0, 1.0])).detach().numpy(),
+            mean["input_feature"][:, 1].detach().numpy(),
         ))
 
     def test_mask(self):
-        encoder = Encoder()
+        encoder = Encoder(torch.nn.Linear(2, 1))
         output = encoder({
+            "processed_input": torch.rand(1, 1),
             "input_feature": torch.arange(2).reshape(2, 1),
-            "reference_features": pad_sequence([torch.arange(3).reshape(3, 1),
-                                                torch.arange(1).reshape(1, 1)
-                                                ])
+            "variables": pad_sequence([torch.rand(3, 1),
+                                       torch.rand(1, 1)
+                                       ])
         })
         self.assertEqual((2, 2), output["input_feature"].shape)
         self.assertEqual((3, 2, 1), output["reference_features"].data.shape)
