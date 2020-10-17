c4235afc0dab64655652e8f02a08e4f39b428bc9
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-10-17 17:59:49 +0900

    Add LazyConfig module

--- mlprogram/entrypoint/configs.py
+++ mlprogram/entrypoint/configs.py
@@ -3,10 +3,57 @@ import yaml
 from typing import Dict, Any
 from pytorch_pfn_extras.config import Config
 from mlprogram.entrypoint.types import types
+from mlprogram.functools import file_cache
+
+
+def with_file_cache(path, config, types):
+    def restore(d):
+        if not isinstance(d, dict):
+            return d
+        if "_type" in d and \
+                d["_type"] == "with_file_cache":
+            t = d.pop("_type")
+            d["type"] = t
+            config = d.pop("config")
+            d = {key: restore(value) for key, value in d.items()}
+            d["config"] = config
+        elif "_type" in d:
+            t = d.pop("_type")
+            d = {key: restore(value) for key, value in d.items()}
+            d["type"] = t
+        return d
+
+    @file_cache(path)
+    def f():
+        return Config(restore(config), types)["/"]
+    return f()
 
 
 def parse_config(configs: Dict[str, Any]) -> Config:
-    return Config(configs, types)
+    _types = {k: v for k, v in types.items()}
+    _types["with_file_cache"] = \
+        lambda path, config: with_file_cache(path, config, _types)
+
+    def convert(d, rename: bool) -> Dict[str, Any]:
+        if not isinstance(d, dict):
+            return d
+        if rename:
+            if "type" in d:
+                t = d.pop("type")
+                d = {key: convert(value, True) for key, value in d.items()}
+                d["_type"] = t
+            else:
+                d = {key: convert(value, True) for key, value in d.items()}
+        else:
+            if "type" in d and d["type"] == "with_file_cache":
+                config = d.pop("config")
+                d = {key: convert(value, False) for key, value in d.items()}
+                d["config"] = convert(config, True)
+            else:
+                d = {key: convert(value, False) for key, value in d.items()}
+        return d
+
+    return Config(convert(configs, False), _types)
 
 
 def load_config(file: str) -> Dict[str, Any]:
--- test/entrypoint/test_configs.py
+++ test/entrypoint/test_configs.py
@@ -1,6 +1,31 @@
 import os
 import tempfile
+from mlprogram.entrypoint.configs import parse_config
 from mlprogram.entrypoint.configs import load_config
+
+
+class TestParseConfig(object):
+    def test_lazy_config(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            path = os.path.join(tmpdir, "cache")
+            config = {
+                "main": {
+                    "type": "with_file_cache",
+                    "path": path,
+                    "config": {
+                        "type": "select",
+                        "key": "key",
+                        "options": {
+                            "key": 0
+                        }
+                    }
+                }
+            }
+            result = parse_config(config)
+            assert result["/main"] == 0
+            config["main"]["config"]["options"]["key"] = 1
+            result = parse_config(config)
+            assert result["/main"] == 0
 
 
 class TestLoadConfig(object):
