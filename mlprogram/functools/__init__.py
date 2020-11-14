93ab480f3f6940b47f6cd4039e7a5f04774325f8
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-11-14 19:39:12 +0900

    Format code

--- mlprogram/functools/__init__.py
+++ mlprogram/functools/__init__.py
@@ -1,6 +1,6 @@
 from mlprogram.functools.cache import FileCache  # noqa
 from mlprogram.functools.cache import file_cache  # noqa
 from mlprogram.functools.functions import Compose  # noqa
+from mlprogram.functools.functions import Identity  # noqa
 from mlprogram.functools.functions import Map  # noqa
 from mlprogram.functools.functions import Sequence  # noqa
-from mlprogram.functools.functions import Identity  # noqa
