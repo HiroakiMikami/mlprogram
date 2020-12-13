26f87e549f19849a1d7c19f1262346f967ad034c
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2020-12-13 09:18:55 +0900

    Enumerate disabled mypy options

--- bin/mypy.bash
+++ bin/mypy.bash
@@ -1,3 +1,26 @@
 #! /bin/bash
 
-mypy --no-site-packages --ignore-missing-imports mlprogram/**/*.py test/**/*.py || exit 1
+mypy \
+    --no-site-packages \
+    --ignore-missing-imports \
+    --follow-imports normal \
+    --allow-redefinition \
+    --warn-redundant-casts \
+    --warn-unused-ignores \
+    mlprogram/**/*.py test/**/*.py || exit 1
+
+
+# --diallow-any-unimported
+# --disallow-any-expr
+# --disallow-any-decorated
+# --disallow-any-explicit
+# --disallow-any-generics
+# --disallow-untyped-calls
+# --disallow-untyped-defs
+# --disallow-subclassing-any
+# --check-untyped-defs
+# --disallow-untyped-decorators
+# --no-implicit-optional
+# --no-warn-no-return
+# --warn-return-any
+
