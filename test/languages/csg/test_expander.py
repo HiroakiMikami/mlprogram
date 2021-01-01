e5c766c0f247437fb97b758d0b2dab9c3cb0020b
Hiroaki Mikami <hiroaki8270+github@gmail.com>
2021-01-01 14:41:16 +0900

    Use fixture in test case

--- test/languages/csg/test_expander.py
+++ test/languages/csg/test_expander.py
@@ -1,9 +1,15 @@
+import pytest
+
 from mlprogram.languages.csg import Circle, Difference, Expander, Reference, Rotation
 
 
+@pytest.fixture
+def expander():
+    return Expander()
+
+
 class TestExpander(object):
-    def test_expand(self):
-        expander = Expander()
+    def test_expand(self, expander):
         assert expander.expand(Circle(1)) == [Circle(1)]
         assert expander.expand(Rotation(1, Circle(1))) == \
             [Circle(1), Rotation(1, Reference(0))]
@@ -16,8 +22,7 @@ class TestExpander(object):
             [Circle(1), Circle(1), Rotation(1, Reference(1)),
              Difference(Reference(0), Reference(2))]
 
-    def test_unexpand(self):
-        expander = Expander()
+    def test_unexpand(self, expander):
         assert expander.unexpand([Circle(1)]) == Circle(1)
         assert expander.unexpand([Rotation(1, Circle(1))]) == \
             Rotation(1, Circle(1))
